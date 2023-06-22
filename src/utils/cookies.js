2432835feaf3fa1bc44f92947092e0dd35814237
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-06-22 14:07:43 +0300

    add window typeguard

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,34 +1,39 @@
 export function setCookies(value, func) {
-  const currentDate = new Date().toISOString().slice(0, 10);
-  window.localStorage.setItem(
-    "cookiesAccepted",
-    JSON.stringify({ accepted: value, date: currentDate })
-  );
-  const d = new Date();
-  d.setTime(d.getTime() + 365 * 24 * 60 * 60 * 1000);
-  const expires = "expires=" + d.toUTCString();
-  document.cookie = `cookiesAccepted=${value}; expires=${expires}; path=/`;
+  if (typeof window !== "undefined") {
+    const currentDate = new Date().toISOString().slice(0, 10);
+    window.localStorage.setItem(
+      "cookiesAccepted",
+      JSON.stringify({ accepted: value, date: currentDate })
+    );
+    const d = new Date();
+    d.setTime(d.getTime() + 365 * 24 * 60 * 60 * 1000);
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = `cookiesAccepted=${value}; expires=${expires}; path=/`;
+  }
   func(false);
 }
 
 export function checkCookies() {
-  const cookiesAccepted = window.localStorage.getItem("cookiesAccepted");
+  if (typeof window !== "undefined") {
+    const cookiesAccepted = window.localStorage.getItem("cookiesAccepted");
 
-  if (cookiesAccepted) {
-    const { accepted, date } = JSON.parse(cookiesAccepted);
-    const currentDate = new Date().toISOString().slice(0, 10);
+    if (cookiesAccepted) {
+      const { accepted, date } = JSON.parse(cookiesAccepted);
+      const currentDate = new Date().toISOString().slice(0, 10);
 
-    if (accepted && date) {
-      const acceptedDate = new Date(date);
-      acceptedDate.setFullYear(acceptedDate.getFullYear() + 1);
+      if (accepted && date) {
+        const acceptedDate = new Date(date);
+        acceptedDate.setFullYear(acceptedDate.getFullYear() + 1);
 
-      if (new Date(currentDate) >= acceptedDate) {
-        return true;
-      } else {
-        return false;
+        if (new Date(currentDate) >= acceptedDate) {
+          return true;
+        } else {
+          return false;
+        }
       }
+    } else {
+      return true;
     }
-  } else {
-    return true;
   }
+  return false;
 }
