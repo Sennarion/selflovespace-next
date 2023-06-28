1e82e47591fb14cf71115df2c0785e731593e2f6
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-06-28 10:36:11 +0300

    change select styles

--- src/components/ui/select/select.jsx
+++ src/components/ui/select/select.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useState, useEffect, useRef } from "react";
-import { gsap } from "gsap";
 import classNames from "classnames";
 import styles from "./select.module.scss";
 
@@ -8,7 +7,6 @@ export default function Select() {
   const [isSelectOpen, setIsSelectOpen] = useState(false);
 
   const $select = useRef(null);
-  const $selectList = useRef(null);
 
   useEffect(() => {
     if (!isSelectOpen) return;
@@ -24,25 +22,6 @@ export default function Select() {
     return () => document.body.removeEventListener("click", closeSelect);
   }, [isSelectOpen]);
 
-  useEffect(() => {
-    const target = $selectList.current;
-
-    if (isSelectOpen) {
-      gsap.to(target, {
-        height: "auto",
-        opacity: "1",
-      });
-
-      return () => {
-        gsap.to(target, {
-          height: "0",
-          opacity: "0",
-        });
-      };
-    }
-    return () => {};
-  }, [isSelectOpen]);
-
   return (
     <div className={styles.selectWrapper} ref={$select}>
       <button
@@ -51,26 +30,24 @@ export default function Select() {
       >
         Nearest events
       </button>
-      <div
+      <ul
         className={classNames(styles.select, {
           [styles["active"]]: isSelectOpen,
         })}
       >
-        <ul className={styles.selectList} ref={$selectList}>
-          <li className={styles.listItem}>
-            <a
-              className={styles.listLink}
-              target="_blank"
-              href="https://docdro.id/bGvkx2b"
-              rel="noreferrer"
-              onClick={() => setIsSelectOpen(false)}
-            >
-              "The Pleasure of Being in Tune with Yourself and Your Body"
-              retreat 19th - 22nd September 2023
-            </a>
-          </li>
-        </ul>
-      </div>
+        <li className={styles.listItem}>
+          <a
+            className={styles.listLink}
+            target="_blank"
+            href="https://docdro.id/bGvkx2b"
+            rel="noreferrer"
+            onClick={() => setIsSelectOpen(false)}
+          >
+            "The Pleasure of Being in Tune with Yourself and Your Body" retreat
+            19th - 22nd September 2023
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
